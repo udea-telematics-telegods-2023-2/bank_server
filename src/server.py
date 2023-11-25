1f46b62fc52cd951c3b923cc0295a2c2c3ca647d
César Montoya <cesara.montoya1@udea.edu.co>
2023-11-25 18:24:09 -0500

    [Feature] Added UDP server integration for encrypted payments, TODO: encryption
    Signed-off-by: César Montoya <cesara.montoya1@udea.edu.co>

--- src/server.py
+++ src/server.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
-
 import logging
-from rsa import key, encrypt, decrypt, newkeys
-from socketserver import ForkingTCPServer, BaseRequestHandler
+from socket import socket
+from socketserver import ForkingTCPServer, ForkingUDPServer, BaseRequestHandler
 from src.bank import Bank
 from sys import argv, exit
+from random import randint
+from threading import Thread
 
 
 class Formatter(logging.Formatter):
+    """
+    Custom log formatter for a more 'pichuki' log output.
+
+    Attributes:
+        FORMATS (dict): A mapping of log levels to their respective formats.
+            The formats include the log level, timestamp, and log message.
+        DATEFMT (str): The date format for the timestamp.
+
+    Methods:
+        format(record): Formats a log record into a string.
+    """
+
     FORMATS = {
         logging.DEBUG: "[DEBUG] %(asctime)s - %(message)s",
         logging.INFO: "[INFO]  %(asctime)s - %(message)s",
@@ -16,27 +29,75 @@ class Formatter(logging.Formatter):
         logging.CRITICAL: "[CRIT]  %(asctime)s - %(message)s",
     }
 
-    def format(self, record):
+    DATEFMT = "%d-%m-%Y %H:%M:%S"
+
+    def format(self, record) -> str:
+        """
+        Formats a log record into a string.
+
+        Args:
+            record (LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The formatted log message.
+
+        Notes:
+            This method overrides the format method in the logging.Formatter class.
+        """
         log_format = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_format, datefmt="%d-%m-%Y %H:%M:%S")
+        formatter = logging.Formatter(log_format, datefmt=self.DATEFMT)
         return formatter.format(record)
 
 
 class Command:
+    """
+    Represents a command for the banking application with associated functions.
+
+    Attributes:
+        __command (str): The command string.
+        __arguments (list): The list of arguments for the command.
+        __fn (callable): The function associated with the command.
+        __error_code (int): The error code resulting from the command execution.
+
+    Methods:
+        debug(): Logs a debug message when the command is executed.
+        no_fn(_: None) -> tuple[int, str]: Default function for commands without an associated function.
+        wrong_args(_: None) -> tuple[int, str]: Function for commands with incorrect arguments.
+        __check_args(args_number: int = 0, fn: callable = no_fn): Checks and sets the associated function based on the command.
+        fn() -> tuple[int, str]: Executes the associated function and returns the result.
+
+    Notes:
+        This class represents a command in the banking application and is associated with specific functions.
+        The associated function is determined based on the command string and the number of arguments.
+    """
+
     def debug(self):
+        """
+        Outputs additional information to the logger.
+        """
         if self.fn != self.no_fn:
             LOGGER.debug(
                 f"{self.__command} executed",
             )
 
     def no_fn(self, _=None) -> tuple[int, str]:
+        """
+        Default function for non-valid commands.
+        """
         return 254, ""
 
     def wrong_args(self, _=None) -> tuple[int, str]:
+        """
+        Default function for wrong_number of arguments.
+        """
         return 253, ""
 
     def __check_args(self, args_number: int = 0, fn=no_fn):
+        """
+        Checks if the number of arguments supplied is correct.
+        """
         if len(self.__arguments) != args_number:
+            self.__arguments = []
             self.__fn = self.wrong_args
         else:
             self.__fn = fn
@@ -71,17 +132,23 @@ class Command:
             case "LOGOUT":
                 self.__check_args(args_number=0, fn=BANK.logout)
 
+            case "PAY":
+                self.__check_args(args_number=4, fn=BANK.pay)
+
             case _:
                 self.__arguments = []
 
     def fn(self) -> tuple[int, str]:
+        """
+        Executes the binded function of the command and returns the error code.
+        """
         self.__error_code, data = self.__fn(*self.__arguments)
         if self.__error_code == 0:
             return 0, data
         return self.__error_code, ""
 
 
-class BankServerHandler(BaseRequestHandler):
+class BankTCPServerHandler(BaseRequestHandler):
     def handle_error(self, error_code: int):
         error_msg = f"Error {error_code}: "
         match error_code:
@@ -91,6 +158,8 @@ class BankServerHandler(BaseRequestHandler):
                 error_msg += "Invalid registration"
             case 3:
                 error_msg += "Insufficient funds"
+            case 251:
+                error_msg += "Unauthorized access"
             case 252:
                 error_msg += "UUID not found"
             case 253:
@@ -102,6 +171,9 @@ class BankServerHandler(BaseRequestHandler):
 
         LOGGER.error(error_msg)
 
+    def check_user(self, connected_uuid: str, cmd_uuid: str) -> bool:
+        return connected_uuid == cmd_uuid
+
     def send_error_code(self, error_code=255):
         self.request.sendall(f"ERR {error_code}\r\n".encode("utf-8"))
 
@@ -109,14 +181,16 @@ class BankServerHandler(BaseRequestHandler):
         self.request.sendall(f"OK {ok_data}\r\n".encode("utf-8"))
 
     def handle_pre_login(self, data: str) -> bool:
+        # Initializes the logged_in flag
         logged_in = False
 
+        # Extracts command and data from input
         command, *arguments = data.split()
         cmd = Command(command, arguments)
         cmd.debug()
         LOGGER.info(f"Command {command} issued by {self.client_address}")
 
-        # If any error
+        # Handle any error and send error code to client
         error_code, cmd_return = cmd.fn()
         if error_code != 0:
             self.handle_error(error_code)
@@ -125,9 +199,17 @@ class BankServerHandler(BaseRequestHandler):
 
         # If the command was login, and there was no error, it means a user logged in
         if command == "LOGIN":
-            username = arguments[0]
+            # Replies with UUID
             self.send_ok_data(cmd_return)
-            LOGGER.info(f"User {username} has logged in")
+
+            # Updates connected_users dictionary
+            global connected_users
+            connected_users[self.client_address] = cmd_return
+
+            # Logs login event and changes the flag
+            self.username = arguments[0]
+            self.uuid = cmd_return
+            LOGGER.info(f"User {self.username} has logged in")
             logged_in = True
             return logged_in
 
@@ -137,38 +219,165 @@ class BankServerHandler(BaseRequestHandler):
 
         return logged_in
 
+    def handle_logout(self, client_address: str) -> bool:
+        # Removes connected_users entry for current session and logs the event
+        global connected_users
+        del connected_users[client_address]
+        LOGGER.info(f"User {self.username} has logged out")
+        return False
+
     def handle_post_login(self, data: str) -> bool:
+        # Extracts command and data from input
         command, *arguments = data.split()
-        LOGGER.info(f"Command {command} issued by {self.client_address}")
+        LOGGER.info(
+            f"Command {command} issued by {self.username}:{self.client_address}"
+        )
+
+        # Sets connected UUID as argument if not given by client
+        if arguments == []:
+            arguments = [self.uuid]
 
         cmd = Command(command, arguments)
         cmd.debug()
 
+        # Check connected user and command UUID
+        if not self.check_user(arguments[0], connected_users[self.client_address]):
+            error_code = 251
+            self.handle_error(error_code)
+            self.send_error_code(error_code)
+            return self.handle_logout(self.client_address)
+
+        # Handle other errors
         error_code, cmd_return = cmd.fn()
         if error_code != 0:
             self.handle_error(error_code)
             self.send_error_code(error_code)
 
+        # If no errors, answer with OK
         self.send_ok_data(cmd_return)
 
-        # And the command was login, it means a user logged in
+        # Handle logout
+        if command == "LOGOUT":
+            return self.handle_logout(self.client_address)
+
         return not (command == "LOGOUT")
 
     def handle(self):
         LOGGER.info(f"Accepted connection from {self.client_address}")
-        SOCKETS_LIST.append(self.client_address)
 
         # Read data from buffer
         data = self.request.recv(4096).decode("utf-8")
 
+        # Checks non-empty message
+        if len(data) <= 2:
+            LOGGER.warning("Empty message")
+            return
+
         # Handle pre-login
         logged = self.handle_pre_login(data)
 
+        # Keep session open if the user is logged in
         while logged:
             # Read data from buffer
-            data = self.request.recv(4096).decode("utf-8")
-            logged = self.handle_post_login(data)
+            data = self.request.recv(4096)
 
+            # Check if client disconnected
+            if not data:
+                break
+            logged = self.handle_post_login(data.decode("utf-8"))
+
+        self.finish()
+
+
+class BankUDPServerHandler(BaseRequestHandler):
+    def handle_error(self, error_code: int):
+        error_msg = f"Error {error_code}: "
+        match error_code:
+            case 1:
+                error_msg += "Invalid login"
+            case 2:
+                error_msg += "Invalid registration"
+            case 3:
+                error_msg += "Insufficient funds"
+            case 251:
+                error_msg += "Unauthorized access"
+            case 252:
+                error_msg += "UUID not found"
+            case 253:
+                error_msg += "Bad arguments"
+            case 254:
+                error_msg += "Unknown command"
+            case 255:
+                error_msg += "Unknown error"
+
+        LOGGER.error(error_msg)
+
+    def encrypt(self, msg: str, n: int) -> str:
+        return msg
+
+    def decrypt(self, msg: str, n: int) -> str:
+        return msg
+
+    def send_encrypted_data(self, conn, to, n: int, encrypted_msg: str):
+        conn.sendto(self.encrypt(f"{encrypted_msg} {n}\r\n", n).encode("utf-8"), to)
+
+    def send_error_code(self, conn, to: socket, n: int, error_code: int = 255):
+        self.send_encrypted_data(conn, to, n, self.encrypt(f"ERR {error_code}", n))
+
+    def send_ok_data(self, conn, to: socket, n: int, ok_data: str = ""):
+        self.send_encrypted_data(conn, to, n, self.encrypt(f"OK {ok_data}", n))
+
+    def handle(self):
+        # Log and get info about the connection
+        LOGGER.info(f"Received UDP message from {self.client_address}")
+        data, conn = self.request
+
+        # Decode the data and decrypt it
+        decoded_data = data.decode("utf-8")
+        if len(decoded_data) <= 1:
+            LOGGER.warning("Empty message")
+            self.finish()
+            return
+        n = decoded_data.split()[-1]
+
+        # Handle bad cypher decode number
+        if not n.isdigit():
+            LOGGER.warning("Bad cypher")
+            self.finish()
+            return
+
+        # Decrypt using the decode number
+        decrypted_data = self.decrypt(decoded_data, n)
+
+        # Generate random n for answer
+        rand_n = randint(1, 26)
+        # n = 0 means no encryption
+        rand_n = 0
+
+        # Validate the only command accepted through UDP
+        if decrypted_data.startswith("PAY"):
+            # Extracts command and data from input
+            command, *arguments = decrypted_data.split()
+
+            # Exclude the decryption number
+            arguments = arguments[:-1]
+            cmd = Command(command, arguments)
+            cmd.debug()
+            LOGGER.info(f"Command {command} issued by {self.client_address}")
+
+            # Handle any error and send error code to client
+            error_code, cmd_return = cmd.fn()
+            if error_code != 0:
+                self.handle_error(error_code)
+                self.send_error_code(conn, self.client_address, rand_n, error_code)
+                self.finish()
+                return
+            # If no error, send OK data
+            self.send_ok_data(conn, self.client_address, rand_n, cmd_return)
+        # If command is not PAY, send bad argument
+        else:
+            error_code = 253
+            self.send_error_code(conn, self.client_address, rand_n, error_code)
         self.finish()
 
 
@@ -196,38 +405,36 @@ if __name__ == "__main__":
     # Enable debugging and configure it
     LOGGER = setup_logger()
 
-    if len(argv) < 3 or len(argv) > 5:
-        LOGGER.info(
-            "Usage: server.py <server_IP> <port> <private_key_path> <public_key_path>"
-        )
+    if len(argv) != 3:
+        LOGGER.info("Usage: server.py <server_IP> <port>")
         exit(1)
 
-    SERVER_IP, PORT = argv[1:3]
+    SERVER_IP, PORT = argv[1:]
 
-    if len(argv) == 3:
-        (PUBLIC_KEY, PRIVATE_KEY) = newkeys(2048)
-    else:
-        PRIVATE_KEY_PATH, PUBLIC_KEY_PATH = argv[3:5]
-
-        with open(PRIVATE_KEY_PATH, "rb") as PRIVATE_KEY_FILE:
-            PRIVATE_KEY = key.PrivateKey.load_pkcs1(PRIVATE_KEY_FILE.read())
-
-        with open(PUBLIC_KEY_PATH, "rb") as PUBLIC_KEY_FILE:
-            PUBLIC_KEY = key.PublicKey.load_pkcs1(PUBLIC_KEY_FILE.read())
-
-    global PUBLIC_KEY_PEM, SOCKETS_LIST, PUBLIC_KEYS_LIST, UUID_LIST, BANK
-    PUBLIC_KEY_PEM = PUBLIC_KEY.save_pkcs1()
-    SOCKETS_LIST = []
-    PUBLIC_KEYS_LIST = {}
-    UUID_LIST = {}
+    global BANK, connected_users
     BANK = Bank()
+    connected_users = {}
 
-    SERVER = ForkingTCPServer((SERVER_IP, int(PORT)), BankServerHandler)
-    LOGGER.info(f"Server listening on {SERVER_IP}:{PORT}")
+    UDP_SERVER = ForkingUDPServer((SERVER_IP, int(PORT)), BankUDPServerHandler)
+    TCP_SERVER = ForkingTCPServer((SERVER_IP, int(PORT)), BankTCPServerHandler)
+    udp_thread = Thread(target=UDP_SERVER.serve_forever)
+    tcp_thread = Thread(target=TCP_SERVER.serve_forever)
 
     try:
-        SERVER.serve_forever()
+        # Start the threads
+        udp_thread.start()
+        LOGGER.info(f"UDP Server listening on {SERVER_IP}:{PORT}")
+        tcp_thread.start()
+        LOGGER.info(f"TCP Server listening on {SERVER_IP}:{PORT}")
+
+        # Wait for both threads to finish
+        tcp_thread.join()
+        udp_thread.join()
+
     except KeyboardInterrupt:
         print("")
         LOGGER.warning("Stopping server, please wait...")
-        SERVER.shutdown()
+
+        # Shutdown both servers
+        TCP_SERVER.shutdown()
+        UDP_SERVER.shutdown()
